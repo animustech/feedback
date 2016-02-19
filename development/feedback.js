4b2331db3d5558813b4be3de047953f9b36e0858
Jonathan Lambert <jonathan@swiftping.co.uk>
2016-02-19 14:02:37 +0000

    Added bootstrap classes to buttons

--- development/feedback.js
+++ development/feedback.js
@@ -29,11 +29,11 @@
 			isDraggable: 			true,
 			onScreenshotTaken: 		function(){},
 			tpl: {
-				description:	'<div id="feedback-welcome"><div class="feedback-logo">Feedback</div><p>Feedback lets you send us suggestions about our products. We welcome problem reports, feature ideas and general comments.</p><p>Start by writing a brief description:</p><textarea id="feedback-note-tmp"></textarea><p>Next we\'ll let you identify areas of the page related to your description.</p><button id="feedback-welcome-next" class="feedback-next-btn feedback-btn-gray">Next</button><div id="feedback-welcome-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
-				highlighter:	'<div id="feedback-highlighter"><div class="feedback-logo">Feedback</div><p>Click and drag on the page to help us better understand your feedback. You can move this dialog if it\'s in the way.</p><button class="feedback-setblackout"><span>Black out</span></button><label class="lower">Black out any personal information.</label><button class="feedback-sethighlight feedback-active"><span>Highlight</span></button><label>Highlight areas relevant to your feedback.</label><div class="feedback-buttons"><button id="feedback-highlighter-next" class="feedback-next-btn feedback-btn-gray">Next</button><button id="feedback-highlighter-back" class="feedback-back-btn feedback-btn-gray">Back</button></div><div class="feedback-wizard-close"></div></div>',
-				overview:		'<div id="feedback-overview"><div class="feedback-logo">Feedback</div><div id="feedback-overview-description"><div id="feedback-overview-description-text"><h3>Description</h3><h3 class="feedback-additional">Automatically Included</h3><div id="feedback-additional-none"><span>None</span></div><div id="feedback-browser-info"><span>Browser Info</span></div><div id="feedback-page-info"><span>Page Info</span></div><div id="feedback-page-structure"><span>Page Structure</span></div></div></div><div id="feedback-overview-screenshot"><h3>Screenshot</h3></div><div class="feedback-buttons"><button id="feedback-submit" class="feedback-submit-btn feedback-btn-blue">Submit</button><button id="feedback-overview-back" class="feedback-back-btn feedback-btn-gray">Back</button></div><div id="feedback-overview-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
-				submitSuccess:	'<div id="feedback-submit-success"><div class="feedback-logo">Feedback</div><p>Thank you for your feedback.</p><button class="feedback-close-btn feedback-btn-blue">OK</button><div class="feedback-wizard-close"></div></div>',
-				submitError:	'<div id="feedback-submit-error"><div class="feedback-logo">Feedback</div><p>Sadly an error occured while sending your feedback. Please try again.</p><button class="feedback-close-btn feedback-btn-blue">OK</button><div class="feedback-wizard-close"></div></div>'
+				description:	'<div id="feedback-welcome"><div class="feedback-logo">Feedback</div><p>Feedback lets you send us suggestions about our products. We welcome problem reports, feature ideas and general comments.</p><p>Start by writing a brief description:</p><textarea id="feedback-note-tmp"></textarea><p>Next we\'ll let you identify areas of the page related to your description.</p><button id="feedback-welcome-next" class="feedback-next-btn btn btn-default">Next</button><div id="feedback-welcome-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
+				highlighter:	'<div id="feedback-highlighter"><div class="feedback-logo">Feedback</div><p>Click and drag on the page to help us better understand your feedback. You can move this dialog if it\'s in the way.</p><button class="feedback-setblackout"><span>Black out</span></button><label class="lower">Black out any personal information.</label><button class="feedback-sethighlight feedback-active"><span>Highlight</span></button><label>Highlight areas relevant to your feedback.</label><div class="feedback-buttons"><button id="feedback-highlighter-next" class="feedback-next-btn btn btn-default">Next</button><button id="feedback-highlighter-back" class="feedback-back-btn btn btn-default">Back</button></div><div class="feedback-wizard-close"></div></div>',
+				overview:		'<div id="feedback-overview"><div class="feedback-logo">Feedback</div><div id="feedback-overview-description"><div id="feedback-overview-description-text"><h3>Description</h3><h3 class="feedback-additional">Automatically Included</h3><div id="feedback-additional-none"><span>None</span></div><div id="feedback-browser-info"><span>Browser Info</span></div><div id="feedback-page-info"><span>Page Info</span></div><div id="feedback-page-structure"><span>Page Structure</span></div></div></div><div id="feedback-overview-screenshot"><h3>Screenshot</h3></div><div class="feedback-buttons"><button id="feedback-submit" class="feedback-submit-btn btn btn-primary">Submit</button><button id="feedback-overview-back" class="feedback-back-btn btn btn-default">Back</button></div><div id="feedback-overview-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
+				submitSuccess:	'<div id="feedback-submit-success"><div class="feedback-logo">Feedback</div><p>Thank you for your feedback.</p><button class="feedback-close-btn btn btn-primary">OK</button><div class="feedback-wizard-close"></div></div>',
+				submitError:	'<div id="feedback-submit-error"><div class="feedback-logo">Feedback</div><p>Sadly an error occured while sending your feedback. Please try again.</p><button class="feedback-close-btn btn btn-primary">OK</button><div class="feedback-wizard-close"></div></div>'
 			},
 			onClose: 				function() {},
 			screenshotStroke:		true,
@@ -45,7 +45,7 @@
 		var _html2canvas = false;
 		if (supportedBrowser) {
 			if(isFeedbackButtonNative) {
-				$('body').append('<button class="feedback-btn feedback-btn-gray">' + settings.initButtonText + '</button>');
+				$('body').append('<button class="feedback-btn btn btn-default">' + settings.initButtonText + '</button>');
 			}
 			$(document).on('click', settings.feedbackButton, function(){
 				if(isFeedbackButtonNative) {
@@ -509,7 +509,8 @@
 
 					if ($('#feedback-note').val().length > 0) {
 						$('#feedback-submit-success,#feedback-submit-error').remove();
-						$('#feedback-overview').hide();
+						// $('#feedback-overview').hide();
+            $('#feedback-submit').addClass('disabled');
 
 						post.img = img;
 						post.note = $('#feedback-note').val();
@@ -531,7 +532,11 @@
 							},
 							error: function(){
 								$('#feedback-module').append(settings.tpl.submitError);
-							}
+							},
+              done: function()
+              {
+                $('#feedback-overview').hide();
+              }
 						});
 					}
 					else {
